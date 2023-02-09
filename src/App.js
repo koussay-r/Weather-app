f0a378c0ec1dfd6140737aa1dccf5d0ecc76b608
koussay-r <koussayr98@gmail.com>
2023-02-09 12:24:18 +0100

    update

--- src/App.js
+++ src/App.js
@@ -6,9 +6,18 @@ import './index.css';
 export const navbarContext=createContext()
 export default function App() {
   const [menuShowed,setMenuShowed]=useState(false)
+  const AllImages=[
+        {id:1,image:sony},
+        {id:2,image:webflow},
+        {id:3,image:samsung},
+        {id:4,image:logitech},
+        {id:5,image:lenovo},
+        {id:6,image:waitrose},
+  ]
   return (
+
     <>
-    <navbarContext.Provider value={[menuShowed,setMenuShowed]} >
+    <navbarContext.Provider value={[menuShowed,setMenuShowed,AllImages]} >
     <BrowserRouter>
     <NavBar/> 
     <HomePgae/>
