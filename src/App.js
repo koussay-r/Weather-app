3641bbf5062ee2daa7736fba100b5037d7a6821f
koussay-r <koussayr98@gmail.com>
2023-02-15 23:57:46 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState } from 'react'
+import {HiOutlineSearch} from "react-icons/hi";
 export default function App() {
-
+  const [bgImage,setBgImage]=useState("")
+  const [inputValue,setInputValue]=useState("")
+  const handleInptValue=(e)=>{
+    setInputValue(e.target.value)
+  }
+  const handleBgImage=async()=>{
+    console.log(process.env.REACT_APP_UNSPLASH_API_KEY)
+    const headers={
+      Authorization: `Client-ID ${process.env.REACT_APP_UNSPLASH_API_KEY}`
+    }
+    try{
+      const response=await axios.get(`https://api.unsplash.com/search/photos?query=${inputValue}`,{headers})
+      console.log(response)
+    }
+    catch(err){
+      console.log(err)
+    }
+  }
   return (
-    <>
-    
-    </>
+    <div className='w-full absolute flex justify-center top-0 bottom-0'>
+      <div className='block bg-red-600 rounded-xl w-[350px] m-auto'>
+        <div className='flex pt-2 justify-center'>
+          <input type={"Text"} value={inputValue} onChange={handleInptValue} className="outline"/><HiOutlineSearch onClick={handleBgImage}/>
+        </div>
+      </div>
+    </div>
   )
 }
